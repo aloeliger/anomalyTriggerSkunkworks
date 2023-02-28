950c0cb36910380c656b38025739119b547dea1d
Andrew Loeliger <loeliger@wisc.edu>
2023-02-28 03:58:44 -0600

    Add crab submission script

---
+++ scripts/submitCrabConfigs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+crab submit -c python/crabConfigs/crabConfig_ephemeralZeroBias1.py
+crab submit -c python/crabConfigs/crabConfig_ephemeralZeroBias2.py
+crab submit -c python/crabConfigs/crabConfig_ephemeralZeroBias3.py
+crab submit -c python/crabConfigs/crabConfig_ephemeralZeroBias4.py
+crab submit -c python/crabConfigs/crabConfig_ephemeralZeroBias5.py
+crab submit -c python/crabConfigs/crabConfig_ephemeralZeroBias6.py
+crab submit -c python/crabConfigs/crabConfig_ephemeralZeroBias7.py
