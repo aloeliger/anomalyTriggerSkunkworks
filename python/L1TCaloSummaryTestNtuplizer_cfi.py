291e8ce338ec3adc31ba35031e1e4433ee056db6
Andrew Loeliger <loeliger@wisc.edu>
2023-02-28 06:19:03 -0600

    Adjust CICADA ntuplizer down to one score from the accepted emulator version.

--- python/L1TCaloSummaryTestNtuplizer_cfi.py
+++ python/L1TCaloSummaryTestNtuplizer_cfi.py
@@ -4,13 +4,9 @@ import FWCore.ParameterSet.Config as cms
 
 L1TCaloSummaryTestNtuplizer = cms.EDAnalyzer('L1TCaloSummaryTestNtuplizer',
                                              scoreSource = cms.InputTag("uct2016EmulatorDigis:anomalyScore"),
-                                             bitAccurateScoreSource = cms.InputTag("uct2016EmulatorDigis:bitAccurateAnomalyScore"),
-                                             precompiledModelScoreSource = cms.InputTag("uct2016EmulatorDigis:precompiledModelAnomalyScore"),
                                              includePUInfo = cms.bool(False),
                                              verboseDebug = cms.bool(False),
-                                             #pvSrc = cms.InputTag("slimmedSecondaryVertices"),
                                              pvSrc = cms.InputTag("offlineSlimmedPrimaryVertices"),
-                                             #pvSrc = cms.InputTag("offlinePrimaryVertices"),
                                              ecalToken = cms.InputTag("l1tCaloLayer1Digis"),
                                              hcalToken = cms.InputTag("l1tCaloLayer1Digis"),
                                              emuRegionsToken = cms.InputTag("simCaloStage2Layer1Digis"),
