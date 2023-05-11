89110f23e12e160c1a33fb83ce333e9a5d83868a
Andrew Loeliger <loeliger@wisc.edu>
2023-05-11 08:14:37 -0500

    Remove old anomalyTriggerSkunkworks plugin

--- python/anomalyTriggerSkunkworks_cfi.py
+++
@@ -1,9 +0,0 @@
-#specific config to define the anomaly trigger skunkworks path
-
-import FWCore.ParameterSet.Config as cms
-
-anomalyTriggerSkunkworks = cms.EDAnalyzer('anomalyTriggerSkunkworks',
-                                          regionalInformation = cms.InputTag("simCaloStage2Layer1Digis"),
-                                          ecalDigis = cms.InputTag("l1tCaloLayer1Digis"),
-                                          hcalDigis = cms.InputTag("l1tCaloLayer1Digis")
-)
