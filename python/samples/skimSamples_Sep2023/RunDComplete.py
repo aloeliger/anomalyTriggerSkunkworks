6913981a613636d10c3f7bda1fa53fd07219c3d7
Andrew Loeliger <loeliger@wisc.edu>
2023-10-04 04:22:02 -0500

    Add more compelte versions of Run A,B,C,D, to the samples

---
+++ python/samples/skimSamples_Sep2023/RunDComplete.py
@@ -0,0 +1,38 @@
+from anomalyDetection.anomalyTriggerSkunkworks.samples.sample import sample
+import os
+
+basePath = '/hdfs/store/user/aloelige/ZeroBias'
+
+filePaths = [
+    f'{basePath}/CICADASkimNtuplization_2023RunD_ZB_29Sep2023/',
+]
+
+theFiles = []
+for filePath in filePaths:
+    for root, dirs, files in os.walk(filePath, topdown=True):
+        for name in files:
+            theFiles.append(os.path.join(root, name))
+
+treeNames = [
+    'CICADAv1ntuplizer/L1TCaloSummaryOutput',
+    'CICADAv2ntuplizer/L1TCaloSummaryOutput',
+    'boostedJetTriggerNtuplizer/boostedJetTrigger',
+    'L1TTriggerBitsNtuplizer/L1TTriggerBits',
+    'uGTModelNtuplizer/uGTModelOutput',
+    'caloStage2EGammaNtuplizer/L1CaloEgammaInformation',
+    'caloStage2JetNtuplizer/L1CaloJetInformation',
+    'caloStage2TauNtuplizer/L1CaloTauInformation',
+    'caloStage2EtSumNtuplizer/L1CaloEtSumInformation',
+    'electronCounter/objectInfo',
+    'jetCounter/objectInfo',
+    'fatJetCounter/objectInfo',
+    'muonCounter/objectInfo',
+    'photonCounter/objectInfo',
+    'tauCounter/objectInfo',
+    'boostedTauCounter/objectInfo',
+]
+
+RunDSample = sample(
+    listOfFiles= theFiles,
+    treeNames=treeNames
+)
