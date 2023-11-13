8b8226c37bf8c1a98a6dd77891af30c162272508
Andrew Loeliger <loeliger@wisc.edu>
2023-11-13 07:27:47 -0600

    Add various unrolled plots to the create and draw scripts

---
+++ scripts/newFramework/createPlots/createUnrolledTriggerObjectEffectPlots.py
@@ -0,0 +1,104 @@
+# !/usr/bin/env python3
+
+import ROOT
+import argparse
+from anomalyDetection.anomalyTriggerSkunkworks.samples.skimSamples_Sep2023.largeRunDEphemeralZeroBias import largeRunDEphemeralZeroBiasSample
+
+from rich.console import Console
+
+console = Console()
+
+def main(args):
+    objects = {
+        'EGamma':{
+            'tree':'caloStage2EGammaNtuplizer/L1CaloEgammaInformation',        
+        },
+        'Jet': {
+            'tree':'caloStage2JetNtuplizer/L1CaloJetInformation',
+        },
+        'Tau':{
+            'tree':'caloStage2TauNtuplizer/L1CaloTauInformation',
+        },
+    }
+
+    if args.CICADAVersion == 1:
+        cicadaThresholds = [0.0, 3.0, 5.0, 6.0, 7.0]
+    elif args.CICADAVersion == 2:
+        cicadaThresholds = [0.0, 8.0, 11.0, 13.0, 15.0]
+
+
+    outputFile = ROOT.TFile(f'/nfs_scratch/aloeliger/anomalyPlotFiles/rootFiles/unrolledTriggerObjectPlotsCICADAv{args.CICADAVersion}.root', 'RECREATE')
+    # We want to unroll in number of objects
+    # and by number of object
+    for typeOfObject in objects:
+        hists = []
+        dfs = []
+        with console.status(f'{typeOfObject}...'):
+            theDataframe = largeRunDEphemeralZeroBiasSample.getNewDataframe(
+                [
+                    f'CICADAv{args.CICADAVersion}ntuplizer/L1TCaloSummaryOutput',
+                    objects[typeOfObject]['tree']
+                ]
+            )
+            for threshold in cicadaThresholds:
+                thresholdDF = theDataframe.Filter(f'anomalyScore > {threshold}')
+                for nObjects in range(1,13):
+                    histoName = f'{typeOfObject}_n{nObjects}_pt_CICADA{int(threshold)}'
+                    unrolledModel = ROOT.RDF.TH1DModel(
+                        histoName,
+                        histoName,
+                        60,
+                        0.0,
+                        120.0
+                    )
+                    objectFilteredDF = thresholdDF.Filter(f'nObjects == {nObjects}')
+                    objectFilteredPlot = objectFilteredDF.Histo1D(unrolledModel, 'ptVector')
+                    hists.append(objectFilteredPlot)
+                    dfs.append(objectFilteredDF)
+                
+                # lets also do the shaping plts by the number of the object
+                for nthObject in range(1,4):
+                    histoName = f'{typeOfObject}_pt_object{nthObject}_CICADA{int(threshold)}'
+                    ptHistModel = ROOT.RDF.TH1DModel(
+                        histoName,
+                        histoName,
+                        60, 
+                        0.0,
+                        120.0
+                    )
+                    specificObjectPtFunction = """
+                    try
+                    {"""+f"""
+                        return ptVector.at({nthObject});"""+"""
+                    }
+                    catch (std::out_of_range const& exc)
+                    {
+                        return -1.0;
+                    }
+                    """
+                    newColumnName = f'object{nthObject}_pt'
+                    columnDefinedDF = thresholdDF.Define(newColumnName, specificObjectPtFunction)
+                    objectFilteredPlot = columnDefinedDF.Histo1D(ptHistModel, newColumnName)
+                    hists.append(objectFilteredPlot)
+                    dfs.append(columnDefinedDF)
+                dfs.append(thresholdDF)
+            dfs.append(theDataframe)
+            for hist in hists:
+                hist.Write()
+        console.log(f'{typeOfObject}', style='green')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="create plots about trigger objects that have been unrolled for certain CICADA thresholds")
+    parser.add_argument(
+        '-v',
+        '--CICADAVersion',
+        default=1,
+        type=int,
+        help='Version to pull the ntuplizer from',
+        choices=[1,2],
+        nargs='?'
+    )
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ scripts/newFramework/drawPlots/drawUnrolledTriggerObjectEffectPlots.py
@@ -0,0 +1,166 @@
+# !/usr/bin/env python3
+
+import ROOT
+import argparse
+from anomalyDetection.anomalyTriggerSkunkworks.samples.skimSamples_Sep2023.largeRunDEphemeralZeroBias import largeRunDEphemeralZeroBiasSample
+import os
+
+from rich.console import Console
+
+console = Console()
+
+def drawUnrolledObjectPlots(theFile, typeOfObject, theThresholds, destinationPath, nObjects=12):
+    for nObject in range(1,nObjects+1):
+        theCanvas = ROOT.TCanvas(
+            f'Unrolled{typeOfObject}_n{nObject}',
+            f'Unrolled{typeOfObject}_n{nObject}',
+        )
+        theCanvas.SetLogy()
+        theLegend = ROOT.TLegend(0.7,0.7,0.9,0.9)
+        for index, threshold in enumerate(theThresholds):
+            theHistoName = f'{typeOfObject}_n{nObject}_pt_CICADA{int(threshold)}'
+            theHisto = theFile.Get(theHistoName)
+
+            theHisto.SetMarkerStyle(20)
+            theHisto.SetMarkerColor(theThresholds[threshold]['color'])
+            theHisto.SetLineColor(theThresholds[threshold]['color'])
+
+            try:
+                theHisto.Scale(1.0/theHisto.Integral())
+            except ZeroDivisionError:
+                pass
+            
+            if index == 0:
+                theHisto.Draw('E1')
+                theHisto.GetXaxis().SetTitle(f'{typeOfObject}'+'p_{T}')
+                theHisto.GetYaxis().SetTitle('Fraction (normalized to 1)')
+                theHisto.SetTitle('')
+            else:
+                theHisto.Draw('SAME E1')
+            
+            theLegend.AddEntry(theHisto, f'CICADA > {threshold}', 'pl')
+        theLegend.Draw()
+
+        cmsLatex = ROOT.TLatex()
+        cmsLatex.SetTextSize(0.05)
+        cmsLatex.SetNDC(True)
+        cmsLatex.SetTextAlign(11)
+        cmsLatex.DrawLatex(0.1,0.92, "#font[61]{CMS} #font[52]{Preliminary}")
+
+        theCanvas.SaveAs(f'{destinationPath}/{typeOfObject}_n{nObject}_pt.png')
+
+def drawNthObjectPlots(theFile, typeOfObject, theThresholds, destinationPath, nObjects=3):
+    for nthObject in range(1,nObjects+1):
+        theCanvas = ROOT.TCanvas(
+            f'{typeOfObject}_pt_object{nthObject}',
+            f'{typeOfObject}_pt_object{nthObject}',
+        )
+        theLegend = ROOT.TLegend(0.7,0.7,0.9,0.9)
+        for index, threshold in enumerate(theThresholds):
+            theHistoName = f'{typeOfObject}_pt_object{nthObject}_CICADA{int(threshold)}'
+            theHisto = theFile.Get(theHistoName)
+
+            theHisto.SetMarkerStyle(20)
+            theHisto.SetMarkerColor(theThresholds[threshold]['color'])
+            theHisto.SetLineColor(theThresholds[threshold]['color'])
+
+            try:
+                theHisto.Scale(1.0/theHisto.Integral())
+            except ZeroDivisionError:
+                pass
+            
+            if index == 0:
+                theHisto.Draw('E1')
+                theHisto.GetXaxis().SetTitle(f'{typeOfObject}'+'p_{T}')
+                theHisto.GetYaxis().SetTitle('Fraction (normalized to 1)')
+                theHisto.SetTitle('')
+            else:
+                theHisto.Draw('SAME E1')
+            
+            theLegend.AddEntry(theHisto, f'CICADA > {threshold}', 'pl')
+        theLegend.Draw()
+
+        cmsLatex = ROOT.TLatex()
+        cmsLatex.SetTextSize(0.05)
+        cmsLatex.SetNDC(True)
+        cmsLatex.SetTextAlign(11)
+        cmsLatex.DrawLatex(0.1,0.92, "#font[61]{CMS} #font[52]{Preliminary}")
+
+        theCanvas.SaveAs(f'{destinationPath}/{typeOfObject}_pt_{nthObject}.png')
+
+def main(args):
+    objects = [
+        'EGamma',
+        'Jet',
+        'Tau'
+    ]
+    if args.CICADAVersion == 1:
+        cicadaThresholds = {
+            0.0: {
+                'color': 40
+            },
+            3.0: {
+                'color': 41
+            },
+            5.0: {
+                'color': 42
+            },
+            6.0: {
+                'color': 46
+            },
+            7.0: {
+                'color': 30
+            },
+        }
+    if args.CICADAVersion == 2:
+        cicadaThresholds = {
+            0.0: {
+                'color': 40
+            },
+            8.0: {
+                'color': 41
+            },
+            11.0: {
+                'color': 42
+            },
+            13.0: {
+                'color': 46
+            },
+            15.0: {
+                'color': 30
+            },
+        }
+    theFile = ROOT.TFile(f'/nfs_scratch/aloeliger/anomalyPlotFiles/rootFiles/unrolledTriggerObjectPlotsCICADAv{args.CICADAVersion}.root')
+    destinationPath = f'/nfs_scratch/aloeliger/anomalyPlotFiles/pngFiles/unrolledTriggerObjectPlotsCICADAv{args.CICADAVersion}/'
+    if not os.path.isdir(destinationPath):
+        os.mkdir(destinationPath)
+    
+    for typeOfObject in objects:
+        drawUnrolledObjectPlots(
+            theFile=theFile,
+            typeOfObject=typeOfObject,
+            theThresholds=cicadaThresholds,
+            destinationPath=destinationPath,
+        )
+        
+        drawNthObjectPlots(
+            theFile=theFile,
+            typeOfObject=typeOfObject,
+            theThresholds=cicadaThresholds,
+            destinationPath=destinationPath
+        )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='draw the trigger object plots')
+    parser.add_argument(
+        '-v',
+        '--CICADAVersion',
+        default=1,
+        type=int,
+        help='Version to pull the ntuplizer from',
+        choices=[1,2],
+        nargs='?'
+    )
+
+    args = parser.parse_args()
+    main(args)
